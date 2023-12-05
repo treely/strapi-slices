3f304d13408107c1f09a4b689dceb2d5476a902f
Lukas Bals <mail@lukasbals.at>
2023-12-05 16:01:53 +0100

    feat: Export Strapi types

--- src/index.tsx
+++ src/index.tsx
@@ -1,1 +1,75 @@
+import IStrapi from './models/strapi/IStrapi';
+import IStrapiData from './models/strapi/IStrapiData';
+import IStrapiResponse from './models/strapi/IStrapiResponse';
+import StrapiAuthor from './models/strapi/StrapiAuthor';
+import StrapiAvatarWithName from './models/strapi/StrapiAvatarWithName';
+import StrapiBanner from './models/strapi/StrapiBanner';
+import StrapiBlogPost from './models/strapi/StrapiBlogPost';
+import StrapiButtonWithVariant from './models/strapi/StrapiButtonWithVariant';
+import StrapiCategory from './models/strapi/StrapiCategory';
+import StrapiContactArea from './models/strapi/StrapiContactArea';
+import StrapiCustomerStory from './models/strapi/StrapiCustomerStory';
+import StrapiDefaultHeader from './models/strapi/StrapiDefaultHeader';
+import StrapiGlobal from './models/strapi/StrapiGlobal';
+import StrapiGlossaryItem from './models/strapi/StrapiGlossaryItem';
+import StrapiHeroCard from './models/strapi/StrapiHeroCard';
+import StrapiImage from './models/strapi/StrapiImage';
+import StrapiImageFormat from './models/strapi/StrapiImageFormat';
+import StrapiImageWithLink from './models/strapi/StrapiImageWithLink';
+import StrapiLink from './models/strapi/StrapiLink';
+import StrapiLinkList from './models/strapi/StrapiLinkList';
+import StrapiLinkPage from './models/strapi/StrapiLinkPage';
+import StrapiLinkWithIcon from './models/strapi/StrapiLinkWithIcon';
+import StrapiLocalization from './models/strapi/StrapiLocalization';
+import StrapiMedia from './models/strapi/StrapiMedia';
+import StrapiMetadata from './models/strapi/StrapiMetadata';
+import StrapiNavMenu from './models/strapi/StrapiNavMenu';
+import StrapiPage from './models/strapi/StrapiPage';
+import StrapiPortfolio from './models/strapi/StrapiPortfolio';
+import StrapiPortfolioCard from './models/strapi/StrapiPortfolioCard';
+import StrapiProject from './models/strapi/StrapiProject';
+import StrapiProjectCard from './models/strapi/StrapiProjectCard';
+import StrapiQuoteCard from './models/strapi/StrapiQuoteCard';
+import StrapiShapesCard from './models/strapi/StrapiShapesCard';
+import StrapiTextCardWithIcons from './models/strapi/StrapiTextCardWithIcons';
+import StrapiTopBanner from './models/strapi/StrapiTopBanner';
+
 export * from './components/SliceRenderer';
+
+export type {
+  IStrapi,
+  IStrapiData,
+  IStrapiResponse,
+  StrapiAuthor,
+  StrapiAvatarWithName,
+  StrapiBanner,
+  StrapiBlogPost,
+  StrapiButtonWithVariant,
+  StrapiCategory,
+  StrapiContactArea,
+  StrapiCustomerStory,
+  StrapiDefaultHeader,
+  StrapiGlobal,
+  StrapiGlossaryItem,
+  StrapiHeroCard,
+  StrapiImage,
+  StrapiImageFormat,
+  StrapiImageWithLink,
+  StrapiLink,
+  StrapiLinkList,
+  StrapiLinkPage,
+  StrapiLinkWithIcon,
+  StrapiLocalization,
+  StrapiMedia,
+  StrapiMetadata,
+  StrapiNavMenu,
+  StrapiPage,
+  StrapiPortfolio,
+  StrapiPortfolioCard,
+  StrapiProject,
+  StrapiProjectCard,
+  StrapiQuoteCard,
+  StrapiShapesCard,
+  StrapiTextCardWithIcons,
+  StrapiTopBanner,
+};

5bb9826b6f0f999e7267439dd9a244c759d8b544
Lukas Bals <mail@lukasbals.at>
2024-02-14 11:16:06 +0100

    fix: Reduce breakpoints by 1px to use exactly the same as Chakra uses

--- src/constants/breakpoints.ts
+++ src/constants/breakpoints.ts
@@ -1,7 +1,7 @@
-export const BREAKPOINT_SM = '320px';
-export const BREAKPOINT_MD = '768px';
-export const BREAKPOINT_LG = '960px';
-export const BREAKPOINT_XL = '1200px';
+export const BREAKPOINT_SM = `${320 - 1}px`;
+export const BREAKPOINT_MD = `${768 - 1}px`;
+export const BREAKPOINT_LG = `${960 - 1}px`;
+export const BREAKPOINT_XL = `${1200 - 1}px`;
 
 export const BREAKPOINT_SM_QUERY = `(max-width: ${BREAKPOINT_SM})`;
 export const BREAKPOINT_MD_QUERY = `(max-width: ${BREAKPOINT_MD})`;

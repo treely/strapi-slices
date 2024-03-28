a2e1f378d59d7958920237440329ca53a1cb4e28
rschiffer <roman.schiffer@gmail.com>
2024-03-28 08:51:14 +0100

    fix(QuoteCards): Fix Button in Header

--- src/slices/QuoteCards/QuoteCards.tsx
+++ src/slices/QuoteCards/QuoteCards.tsx
@@ -16,6 +16,7 @@ import StrapiHeroCard from '../../models/strapi/StrapiHeroCard';
 import strapiLinkUrl from '../../utils/strapiLinkUrl';
 import StrapiImage from '../../models/strapi/StrapiImage';
 import convertToKebabCase from '../../utils/convertToKebabCase';
+import { useRouter } from 'next/router';
 
 interface QuoteCardsSlice extends StrapiDefaultHeader {
   cards: StrapiQuoteCard[];
@@ -28,110 +29,114 @@ export interface QuoteCardsProps {
 
 export const QuoteCards: React.FC<QuoteCardsProps> = ({
   slice,
-}: QuoteCardsProps) => (
-  <>
-    <Box
-      id={convertToKebabCase(slice.title)}
-      position="relative"
-      paddingTop="28"
-      paddingBottom={!!slice.hero ? '80' : '28'}
-    >
-      {slice.shapes && slice.shapes.length === 2 && (
-        <>
-          <Shape
-            shape={
-              <Image
-                src={strapiMediaUrl(slice.shapes[0].img, 'small')}
-                alt={slice.shapes[0].alt}
-                fill
-                style={{ objectFit: slice.shapes[0].objectFit || 'cover' }}
-              />
-            }
-            top="0"
-            right="0"
-            size="xs"
-            radius="bottom-left"
-          />
-          <Shape
-            shape={
-              <Image
-                src={strapiMediaUrl(slice.shapes[1].img, 'small')}
-                alt={slice.shapes[1].alt}
-                fill
-                style={{ objectFit: slice.shapes[1].objectFit || 'cover' }}
-              />
-            }
-            bottom="0"
-            left="0"
-            radius="top-right"
-          />
-        </>
-      )}
-      <Wrapper>
-        <Box maxW="3xl">
-          <DefaultSectionHeader
-            tagline={slice.tagline}
-            title={slice.title}
-            text={slice.text}
-          />
-        </Box>
-        <SimpleGrid
-          columns={2}
-          gap="20"
-          rowGap="6"
-          mt="16"
-          minChildWidth={['100%', null, '16rem']}
-        >
-          {slice.cards.map((card) => (
-            <Box key={card.id} width="full" maxWidth="2xl">
-              <QuoteCard
-                key={card.id}
-                avatar={{
-                  name: card.avatar.name,
-                  description: card.avatar.description,
-                  image: (
-                    <Image
-                      src={strapiMediaUrl(card.avatar.image.img, 'small')}
-                      alt={card.avatar.image.alt}
-                      fill
-                      style={{
-                        objectFit: card.avatar.image.objectFit || 'cover',
-                      }}
-                    />
-                  ),
-                }}
-                text={card.text}
-              />
-            </Box>
-          ))}
-        </SimpleGrid>
-      </Wrapper>
-    </Box>
-    {slice.hero && (
-      <Box marginTop="-40" paddingBottom="28">
-        <Wrapper>
-          <HeroCard
-            title={slice.hero.title}
-            subTitle={slice.hero.subTitle}
-            link={
-              slice.hero.button && {
-                text: slice.hero.button.text,
-                onClick: () => strapiLinkUrl(slice.hero?.button),
-              }
-            }
-            image={
-              slice.hero.image && (
+}: QuoteCardsProps) => {
+  const { push } = useRouter();
+
+  return (
+    <>
+      <Box
+        id={convertToKebabCase(slice.title)}
+        position="relative"
+        paddingTop="28"
+        paddingBottom={!!slice.hero ? '80' : '28'}
+      >
+        {slice.shapes && slice.shapes.length === 2 && (
+          <>
+            <Shape
+              shape={
                 <Image
-                  src={strapiMediaUrl(slice.hero.image.img, 'xLarge')}
-                  alt={slice.hero.image.alt}
+                  src={strapiMediaUrl(slice.shapes[0].img, 'small')}
+                  alt={slice.shapes[0].alt}
                   fill
-                  style={{ objectFit: slice.hero.image.objectFit || 'cover' }}
+                  style={{ objectFit: slice.shapes[0].objectFit || 'cover' }}
                 />
-              )
-            }
-          />
+              }
+              top="0"
+              right="0"
+              size="xs"
+              radius="bottom-left"
+            />
+            <Shape
+              shape={
+                <Image
+                  src={strapiMediaUrl(slice.shapes[1].img, 'small')}
+                  alt={slice.shapes[1].alt}
+                  fill
+                  style={{ objectFit: slice.shapes[1].objectFit || 'cover' }}
+                />
+              }
+              bottom="0"
+              left="0"
+              radius="top-right"
+            />
+          </>
+        )}
+        <Wrapper>
+          <Box maxW="3xl">
+            <DefaultSectionHeader
+              tagline={slice.tagline}
+              title={slice.title}
+              text={slice.text}
+            />
+          </Box>
+          <SimpleGrid
+            columns={2}
+            gap="20"
+            rowGap="6"
+            mt="16"
+            minChildWidth={['100%', null, '16rem']}
+          >
+            {slice.cards.map((card) => (
+              <Box key={card.id} width="full" maxWidth="2xl">
+                <QuoteCard
+                  key={card.id}
+                  avatar={{
+                    name: card.avatar.name,
+                    description: card.avatar.description,
+                    image: (
+                      <Image
+                        src={strapiMediaUrl(card.avatar.image.img, 'small')}
+                        alt={card.avatar.image.alt}
+                        fill
+                        style={{
+                          objectFit: card.avatar.image.objectFit || 'cover',
+                        }}
+                      />
+                    ),
+                  }}
+                  text={card.text}
+                />
+              </Box>
+            ))}
+          </SimpleGrid>
         </Wrapper>
       </Box>
-    )}
-  </>
-);
+      {slice.hero && (
+        <Box marginTop="-40" paddingBottom="28">
+          <Wrapper>
+            <HeroCard
+              title={slice.hero.title}
+              subTitle={slice.hero.subTitle}
+              link={
+                slice.hero.button && {
+                  text: slice.hero.button.text,
+                  onClick: () => push(strapiLinkUrl(slice.hero?.button)),
+                }
+              }
+              image={
+                slice.hero.image && (
+                  <Image
+                    src={strapiMediaUrl(slice.hero.image.img, 'xLarge')}
+                    alt={slice.hero.image.alt}
+                    fill
+                    style={{ objectFit: slice.hero.image.objectFit || 'cover' }}
+                  />
+                )
+              }
+            />
+          </Wrapper>
+        </Box>
+      )}
+    </>
+  );
+};

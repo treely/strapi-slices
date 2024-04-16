90fafa2a695a0696e665762a29676f1a737895b1
lialila <uche0790@gmail.com>
2024-04-16 09:51:13 +0200

    fix(IconGrid): Update the breakpoints of the grid

--- src/slices/IconGrid/IconGrid.tsx
+++ src/slices/IconGrid/IconGrid.tsx
@@ -31,76 +31,91 @@ export interface IconGridProps {
   };
 }
 
-export const IconGrid: React.FC<IconGridProps> = ({ slice }: IconGridProps) => (
-  <DefaultSectionContainer>
-    <Wrapper>
-      {slice.title ? (
-        <>
-          <Flex alignItems="center" flexDirection="column">
-            <DefaultSectionHeader
-              tagline={slice.tagline}
-              title={slice.title}
-              text={slice.subTitle}
-              taglineProps={{ maxW: '2xl', textAlign: 'center' }}
-              titleProps={{ maxW: '3xl', textAlign: 'center' }}
-              textProps={{ maxW: '3xl', textAlign: 'center' }}
-            />
-          </Flex>
-          <Spacer h="20" />
-        </>
-      ) : (
-        <></>
-      )}
+export const IconGrid: React.FC<IconGridProps> = ({ slice }: IconGridProps) => {
+  const columns = () => {
+    if (slice.iconsWithTextAndButton.length === 2) {
+      return [1, null, null, 2, 2];
+    }
+    if (slice.iconsWithTextAndButton.length === 1) {
+      return 1;
+    }
+    return [1, null, null, 2, 3];
+  };
 
-      <SimpleGrid
-        columns={[1, null, null, 2, 3]}
-        gap="16"
-        rowGap={['16', null, null, '20']}
-      >
-        {slice.iconsWithTextAndButton.map((iconWithTextAndButton) => (
-          <Box key={iconWithTextAndButton.id}>
-            <Box
-              margin="0 auto"
-              backgroundColor="primary.50"
-              borderRadius="full"
-              display="flex"
-              alignItems="center"
-              justifyContent="space-around"
-              width="6.5rem"
-              height="6.5rem"
-            >
-              <Box position="absolute" width="12" height="12">
-                <Image
-                  src={strapiMediaUrl(iconWithTextAndButton.icon.img, 'xSmall')}
-                  alt={iconWithTextAndButton.icon.alt}
-                  fill
-                  style={{
-                    objectFit:
-                      iconWithTextAndButton.icon.objectFit || 'contain',
-                  }}
-                />
+  return (
+    <DefaultSectionContainer>
+      <Wrapper>
+        {slice.title ? (
+          <>
+            <Flex alignItems="center" flexDirection="column">
+              <DefaultSectionHeader
+                tagline={slice.tagline}
+                title={slice.title}
+                text={slice.subTitle}
+                taglineProps={{ maxW: '2xl', textAlign: 'center' }}
+                titleProps={{ maxW: '3xl', textAlign: 'center' }}
+                textProps={{ maxW: '3xl', textAlign: 'center' }}
+              />
+            </Flex>
+            <Spacer h="20" />
+          </>
+        ) : (
+          <></>
+        )}
+
+        <SimpleGrid
+          columns={columns()}
+          gap={slice.iconsWithTextAndButton.length === 2 ? '14' : '16'}
+          rowGap={['16', null, null, '20']}
+        >
+          {slice.iconsWithTextAndButton.map((iconWithTextAndButton) => (
+            <Box key={iconWithTextAndButton.id}>
+              <Box
+                margin="0 auto"
+                backgroundColor="primary.50"
+                borderRadius="full"
+                display="flex"
+                alignItems="center"
+                justifyContent="space-around"
+                width="6.5rem"
+                height="6.5rem"
+              >
+                <Box position="absolute" width="12" height="12">
+                  <Image
+                    src={strapiMediaUrl(
+                      iconWithTextAndButton.icon.img,
+                      'xSmall'
+                    )}
+                    alt={iconWithTextAndButton.icon.alt}
+                    fill
+                    style={{
+                      objectFit:
+                        iconWithTextAndButton.icon.objectFit || 'contain',
+                    }}
+                  />
+                </Box>
               </Box>
+              <Heading size="xl" textAlign="center" mb="4" mt="8">
+                {iconWithTextAndButton.title}
+              </Heading>
+              <Text size="mdRegularNormal" textAlign="center">
+                {iconWithTextAndButton.text}
+              </Text>
+              {iconWithTextAndButton.button && (
+                <Box textAlign="center">
+                  <StrapiLinkButton
+                    key={iconWithTextAndButton.button.id}
+                    mt="8"
+                    size="md"
+                    variant="outline"
+                    link={iconWithTextAndButton.button}
+                  />
+                </Box>
+              )}
             </Box>
-            <Heading size="xl" textAlign="center" mb="4" mt="8">
-              {iconWithTextAndButton.title}
-            </Heading>
-            <Text size="mdRegularNormal" textAlign="center">
-              {iconWithTextAndButton.text}
-            </Text>
-            {iconWithTextAndButton.button && (
-              <Box textAlign="center">
-                <StrapiLinkButton
-                  key={iconWithTextAndButton.button.id}
-                  mt="8"
-                  size="md"
-                  variant="outline"
-                  link={iconWithTextAndButton.button}
-                />
-              </Box>
-            )}
-          </Box>
-        ))}
-      </SimpleGrid>
-    </Wrapper>
-  </DefaultSectionContainer>
-);
+          ))}
+        </SimpleGrid>
+      </Wrapper>
+    </DefaultSectionContainer>
+  );
+};

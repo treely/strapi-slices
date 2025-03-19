eaa0a5e168b796f3f2913978aefc9feef82c7f0e
lialila <uche0790@gmail.com>
2025-03-19 16:34:23 +0100

    fix: increase population level to 5

--- src/constants/strapi.ts
+++ src/constants/strapi.ts
@@ -2,6 +2,6 @@ export const STRAPI_URI =
   process.env.NEXT_PUBLIC_STRAPI_URI || 'http://127.0.0.1:1337';
 
 export const STRAPI_DEFAULT_PAGE_SIZE = '100';
-export const STRAPI_DEFAULT_POPULATE_DEPTH = '4';
+export const STRAPI_DEFAULT_POPULATE_DEPTH = '5';
 
 export const STRAPI_FALLBACK_LOCALE = 'en';
